f4c417c2c8d0540ccc4011f08976c9b4eadce143
meikhaill <meikhailmajor9@gmail.com>
2024-02-10 13:27:39 -0700

    Added users.txt file, modified index, added contact page

--- app.rb
+++ app.rb
@@ -1,8 +1,23 @@
 require 'sinatra'
+require_relative 'models/user'
 
 # Define a route for the root URL
 get '/' do
   erb :index
+end
+
+# use Rack::Auth::Basic, "Restricted Area" do |username, password|
+#     File.foreach("users.txt") do |line|
+#       stored_username, stored_password = line.chomp.split(":")
+#        if username == stored_username && password == stored_password
+#         authenticated = true
+#         break
+#        end
+#     end
+#     false
+# end
+get '/contact' do
+  "Contact me!"
 end
 
 get '/about' do
